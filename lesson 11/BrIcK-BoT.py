da7d8f56c356786c78ceafefda60789c4fc11a64
EvanZMercado <43785627+EvanZMercado@users.noreply.github.com>
2018-12-19 12:43:47 -0500

    Add files via upload

---
+++ lesson 11/BrIcK-BoT.py
@@ -0,0 +1,10 @@
+print('-'*65)
+print('Iz iT BrIcK BoT?')
+print()
+Fahrenheit = input('Temperature in Fahrenheit: ')
+Fahrenheit = int(Fahrenheit)
+if Fahrenheit > 60 :
+	print('ItZ nOt bRiCk, EnJoY yA DaY!')
+else:
+	print('iTz bRiCk, WeAR Ya JaCKeT!')
+print('-'*65)
---
+++ lesson 11/Narrative-Bot.py
@@ -0,0 +1,13 @@
+print('-'*65)
+print('Narrative Bot:')
+print()
+Name = input('Student Name: ')
+Grade = input('Grade: ')
+Name = int(Name)
+Grade = int(Grade)
+
+if Grade > 65 :
+	print('')
+else:
+	print('')
+print('-'*65)
